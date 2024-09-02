8d5583adcc38ac013bb2f64d9aaf9d770e7217e9
Matheus Marciano <mm.leitea@outlook.com>
2024-09-02 15:55:41 -0300

    🐛 Fix npm i host / remote permissions conflict

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,5 +2,7 @@ export $(cat dev-env.config)
 
 export GIT_USER=$(git config --global user.name)
 export GIT_EMAIL=$(git config --global user.email)
+export D_UID=$(id -u)
+export D_GID=$(id -g)
 
 docker compose up -d --build
