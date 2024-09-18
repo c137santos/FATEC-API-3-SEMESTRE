d395b1085fdd031e51939cbc8008febab3f7f1e0
PortifoliodoEdu <eduardolima.contatoprofissional@gmail.com>
2024-09-18 08:08:17 -0300

    ✨ Desenvolve layout da Home com dados mockados

--- backend/src/main/java/com/group/backend/controller/ClientController.java
+++
@@ -1,8 +0,0 @@
-package com.group.backend.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ClientController {
-
-}
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,62 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import SideBar from '@/components/SideBar.vue'; // Importando a Sidebar
 </script>
 
 <template>
   <div id="app">
-  
-    <RouterView />
+    <!-- Barra de navegação lateral (Sidebar) -->
+    <SideBar />
+
+    <!-- Conteúdo principal -->
+    <div class="main-content">
+      <!-- Título do aplicativo -->
+      <header class="header">
+        <h1 class="cerberus-title">Cérberus</h1>
+      </header>
+
+      <!-- O conteúdo da rota atual será carregado aqui -->
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <style scoped>
 #app {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100vh;
   display: flex;
-  flex-direction: column;
+  height: 100vh;
   overflow-x: hidden;
+  background-color: #ffffff;
+}
+
+.header {
+  top: 10;
+  flex-grow: 1;
+  padding-left: 80px;
+  background-color: #ffffff;
+}
+
+
+.cerberus-title {
+  font-size: 32px;
+  color: #65558F;
+  margin: 0;
+}
+
+.main-content {
+  padding-left: 80px; /* Mantém o espaçamento da sidebar */
+  flex-grow: 1;
+  background-color: #ffffff; /* Cor de fundo uniforme */
+} 
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 80px;
+  background-color: #ECE6F0; /* Cor de fundo uniforme */
+  box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
 }
 
 body, html {
@@ -25,6 +64,6 @@ body, html {
   padding: 0;
   width: 100%;
   height: 100%;
-  overflow-x: hidden; 
+  overflow-x: hidden;
 }
 </style>
