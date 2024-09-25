4f21c556279be741819c1463c3882cefea741c90
santosc137 <cs@moray.ai>
2024-09-25 17:30:36 -0300

    🔥 Retirando comentarios

--- frontend/src/views/TagView.vue
+++ frontend/src/views/TagView.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <!-- Componente para cadastrar uma nova tag -->
     <AdicionarTag @nova-tag="adicionarTag" />
-
-    <!-- Componente para listar as tags -->
     <ListaTags :tags="tags" />
   </div>
 </template>
@@ -20,25 +17,23 @@ export default {
   },
   data() {
     return {
-      tags: []  // Armazena a lista de tags
+      tags: [] 
     };
   },
   mounted() {
-    this.carregarTags();  // Carrega as tags do backend assim que o componente é montado
+    this.carregarTags();
   },
   methods: {
-    // Função para carregar as tags do backend
     async carregarTags() {
       try {
         const response = await fetch('http://localhost:8080/tags/listar');
         const tags = await response.json();
-        this.tags = tags;  // Armazena as tags carregadas no array `tags`
+        this.tags = tags; 
       } catch (error) {
         console.error('Erro ao carregar tags:', error);
       }
     },
 
-    // Adiciona uma nova tag à lista sem recarregar a página
     adicionarTag(novaTag) {
       this.tags.push(novaTag);
     }
