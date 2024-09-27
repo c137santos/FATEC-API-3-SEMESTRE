0f3596f90b205332fcdb25091789f94810fd6c35
marilia_moraes <marilia.bm01@gmail.com>
2024-09-27 19:38:55 -0300

    ✨ Listar apis publicas

--- backend/src/main/java/com/group/backend/entity/ApiPublica.java
+++ backend/src/main/java/com/group/backend/entity/ApiPublica.java
@@ -1,34 +1,41 @@
 package com.group.backend.entity;
+
 import jakarta.persistence.*;
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "api_publica")
+@NoArgsConstructor
 public class ApiPublica {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "api_id")
+    @Getter
     private Long id;
 
     @Column(name = "api_nome", nullable = false)
+    @Getter
     private String nome;
 
     @Column(name = "api_frequencia", nullable = false)
+    @Getter
     private String frequencia;
 
     @Column(name = "api_descricao")
+    @Getter
     private String descricao;
 
     @Column(name = "api_active")
+    @Getter
     private Boolean active;
 
     @Column(name = "api_url", length = 2048)
+    @Getter
     private String url;
-    
-    public ApiPublica() {}
 
-    public ApiPublica(String nome, String frequencia, String descricao, Boolean active, String url, LocalDate dataFuturaCaptura) {
+    public ApiPublica(String nome, String frequencia, String descricao, Boolean active, String url) {
         this.nome = nome;
         this.frequencia = frequencia;
         this.descricao = descricao;
