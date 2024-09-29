a972d87b165a6c5794160528e3d36401240af776
Marília Borgo de Moraes <marilia.bm01@gmail.com>
2024-09-29 13:48:35 -0300

    Marilia/lista result api da api (#47)
    * ✨ Lista result api
    
    * ♻️ Tira o axios e usa o fetch
    
    * ♻️ Melhora o botao de fechar
    
    * ♻️ Sobrou comentarios

--- backend/src/main/java/com/group/backend/controller/ApiController.java
+++ backend/src/main/java/com/group/backend/controller/ApiController.java
@@ -6,12 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.group.backend.domain.ApiRepository;
 import com.group.backend.entity.ApiPublica;
+
+import com.group.backend.domain.ResultApiRepository;
+import com.group.backend.entity.ResultApi;
+
 
 
 @RequestMapping("/apis")
@@ -20,9 +25,12 @@ import com.group.backend.entity.ApiPublica;
 public class ApiController {
 
     private final ApiRepository apiRepository;
+    private final ResultApiRepository resultApiRepository;
 
-    public ApiController(ApiRepository apiRepository) {
+    public ApiController(ApiRepository apiRepository, ResultApiRepository resultApiRepository) {
         this.apiRepository = apiRepository;
+        this.resultApiRepository = resultApiRepository;
+
     }
 
     @GetMapping("listar")
@@ -41,4 +49,14 @@ public class ApiController {
         return ResponseEntity.ok(novaApi);
     }
 
+    @GetMapping("resultados")
+    public ResponseEntity<List<ResultApi>> getAllResultsByApiId(@RequestParam Long apiId) {
+        List<ResultApi> results = resultApiRepository.findAllByApiId(apiId);
+        
+        if (!results.isEmpty()) {
+            return ResponseEntity.ok(results);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
