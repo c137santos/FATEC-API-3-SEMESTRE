ba18f60be1bc51a9bcfc18ae4af00193a29ebe07
GuilhermebJunqueira <143050492+GuilhermebJunqueira@users.noreply.github.com>
2024-10-11 18:57:40 -0300

    Update ApiController.java

--- backend/src/main/java/com/group/backend/controller/ApiController.java
+++ backend/src/main/java/com/group/backend/controller/ApiController.java
@@ -6,16 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.group.backend.entity.ApiPublica;
 import com.group.backend.domain.ApiPublicaRepository;
-
 import com.group.backend.domain.ResultApiRepository;
+import com.group.backend.entity.ApiPublica;
 import com.group.backend.entity.ResultApi;
+
+
 
 @RequestMapping("/apis")
 @RestController
@@ -32,12 +33,12 @@ public class ApiController {
 
     @GetMapping("listar")
     public ResponseEntity<List<ApiPublica>> listarApis() {
-        List<ApiPublica> apis = apiRepository.findAll();
-        for (ApiPublica apiP : apis) {
-            System.out.println(apiP);
-        }
-        
-        return ResponseEntity.ok(apis);
+    List<ApiPublica> apis = apiRepository.findAll();
+    for (ApiPublica apiP : apis) {
+        System.out.println(apiP);
+    }
+    
+    return ResponseEntity.ok(apis);
     }
 
     @PostMapping("cadastrar")
