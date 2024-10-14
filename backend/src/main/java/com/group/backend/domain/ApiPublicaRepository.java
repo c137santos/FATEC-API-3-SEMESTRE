006db2721835b500a6ccf5d4811b202c5611564b
Matheus Marciano <mm.leitea@outlook.com>
2024-10-14 16:51:21 -0300

    🐛 Fix CRON saving data on same day

--- backend/src/main/java/com/group/backend/domain/ApiPublicaRepository.java
+++ backend/src/main/java/com/group/backend/domain/ApiPublicaRepository.java
@@ -10,10 +10,10 @@ import com.group.backend.entity.ApiPublica;
 
 @Repository
 public interface ApiPublicaRepository extends JpaRepository<ApiPublica, Long> {
-	@Query(value = "SELECT * FROM api_publica WHERE api_frequencia='diaria' AND api_active=true", nativeQuery = true)
+	@Query(value = "SELECT * FROM api_publica WHERE api_frequencia='diariamente' AND api_active=true", nativeQuery = true)
     ArrayList<ApiPublica> getDailyList();
-	@Query(value = "SELECT * FROM api_publica WHERE api_frequencia='semanal' AND api_active=true", nativeQuery = true)
+	@Query(value = "SELECT * FROM api_publica WHERE api_frequencia='semanalmente' AND api_active=true", nativeQuery = true)
     ArrayList<ApiPublica> getWeeklyList();
-	@Query(value = "SELECT * FROM api_publica WHERE api_frequencia='mensal' AND api_active=true", nativeQuery = true)
+	@Query(value = "SELECT * FROM api_publica WHERE api_frequencia='mensalmente' AND api_active=true", nativeQuery = true)
     ArrayList<ApiPublica> getMonthlyList();
 }
