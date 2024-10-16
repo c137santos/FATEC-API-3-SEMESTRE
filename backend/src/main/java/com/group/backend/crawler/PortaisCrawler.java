714ac544e7a814d7ced70dc132920cae3de7b3d3
marilia_moraes <marilia.bm01@gmail.com>
2024-10-16 10:11:05 -0300

    ✨ Conecta com o banco para pegar urls

--- backend/src/main/java/com/group/backend/crawler/PortaisCrawler.java
+++ backend/src/main/java/com/group/backend/crawler/PortaisCrawler.java
@@ -1,16 +1,38 @@
 package com.group.backend.crawler;
 
-public class PortaisCrawler {
+import com.group.backend.domain.PortalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.stereotype.Component;
 
-    public static void main(String[] args) {
-        String exampleUrl = "https://thenewscc.beehiiv.com/";
+import java.util.List;
 
+@Component
+public class PortaisCrawler implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final PortalRepository portalRepository;
+
+    @Autowired
+    public PortaisCrawler(PortalRepository portalRepository) {
+        this.portalRepository = portalRepository;
+    }
+
+    // Este método será chamado automaticamente quando a aplicação estiver pronta
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
         ControllerCrawler controllerCrawler = new ControllerCrawler();
 
-        try {
-            controllerCrawler.startCrawlWithSeed(exampleUrl);
-        } catch (Exception e) {
-            e.printStackTrace();
+        List<String> urls = portalRepository.findAllUrlsBy();
+
+        for (String url : urls) {
+            try {
+                System.out.println("Iniciando crawl para: " + url);
+                controllerCrawler.startCrawlWithSeed(url);
+            } catch (Exception e) {
+                System.err.println("Erro ao iniciar o crawler para a URL: " + url);
+                e.printStackTrace();
+            }
         }
     }
 }
--- backend/src/main/java/com/group/backend/domain/PortalRepository.java
+++ backend/src/main/java/com/group/backend/domain/PortalRepository.java
@@ -1,11 +1,14 @@
 package com.group.backend.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 import com.group.backend.entity.Portal;
 
 @Repository
 public interface PortalRepository extends JpaRepository<Portal, Long> {
-    
+    @Query("SELECT p.url FROM Portal p")
+    List<String> findAllUrlsBy();
 }
