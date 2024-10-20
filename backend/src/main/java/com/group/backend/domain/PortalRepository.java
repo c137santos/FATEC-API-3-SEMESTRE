68dbcd6a4e756ac4ada39d4b848603b3894c6d6c
santosc137 <cs@moray.ai>
2024-10-20 16:44:15 -0300

    🐛 Definindo para uso de PortalRepository filter by true for Portal Ativo

--- backend/src/main/java/com/group/backend/domain/PortalRepository.java
+++ backend/src/main/java/com/group/backend/domain/PortalRepository.java
@@ -1,7 +1,6 @@
 package com.group.backend.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -9,6 +8,5 @@ import com.group.backend.entity.Portal;
 
 @Repository
 public interface PortalRepository extends JpaRepository<Portal, Long> {
-    @Query("SELECT p.url FROM Portal p WHERE p.ativo = true")
-    List<String> findAllUrlsBy();
+    List<Portal> findByAtivoTrue();
 }
