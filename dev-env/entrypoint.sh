a9bdd750c53f726f327ae422951d0110c2850651
Matheus Marciano <mm.leitea@outlook.com>
2024-10-27 03:48:42 -0300

    🧑‍💻 Update dev-container to run as ubuntu not root

--- dev-env/entrypoint.sh
+++ dev-env/entrypoint.sh
@@ -1,12 +1,9 @@
-git config --global --add safe.directory /app
+sudo service postgresql start
+sudo psql -U postgres -c "CREATE USER ${DB_USERNAME} WITH SUPERUSER PASSWORD '${DB_PASSWORD}';"
+sudo createdb -U postgres $DB_NAME
 
-sed -i 's/peer/trust/g' /etc/postgresql/16/main/pg_hba.conf
 
-service postgresql start
-psql -U postgres -c "CREATE USER ${DB_USERNAME} WITH SUPERUSER PASSWORD '${DB_PASSWORD}';"
-createdb -U postgres $DB_NAME
-
-echo "export DB_URL=\"jdbc:postgresql://localhost:5432/${DB_NAME}?user=${DB_USERNAME}&password=${DB_USERNAME}\"" >> /root/.zshrc
+echo "export DB_URL=\"jdbc:postgresql://localhost:5432/${DB_NAME}?user=${DB_USERNAME}&password=${DB_USERNAME}\"" >> /home/ubuntu/.zshrc
 
 npm i
 zsh
