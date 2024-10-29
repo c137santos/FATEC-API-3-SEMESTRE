5582f38290e0091a144868aa24a5e180ea1baced
Matheus Marciano <mm.leitea@outlook.com>
2024-10-29 19:32:13 -0300

    🧑‍💻 Fix dabase connection to host not working

--- dev-env/entrypoint.sh
+++ dev-env/entrypoint.sh
@@ -2,8 +2,8 @@ sudo service postgresql start
 sudo psql -U postgres -c "CREATE USER ${DB_USERNAME} WITH SUPERUSER PASSWORD '${DB_PASSWORD}';"
 sudo createdb -U postgres $DB_NAME
 
+echo "export DB_URL=\"jdbc:postgresql://localhost:5432/${DB_NAME}?user=${DB_USERNAME}&password=${DB_PASSWORD}\"" >> /home/ubuntu/.zshrc
 
-echo "export DB_URL=\"jdbc:postgresql://localhost:5432/${DB_NAME}?user=${DB_USERNAME}&password=${DB_USERNAME}\"" >> /home/ubuntu/.zshrc
 
 npm i
 zsh
