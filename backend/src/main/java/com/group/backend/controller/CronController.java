d0867734ee1e2198dab35b34b710db98d5d82b7e
Matheus Marciano <mm.leitea@outlook.com>
2024-10-29 11:25:31 -0300

    ♻️ Arrumar divisão de objetos entre CronApi e ApiConsumer

--- backend/src/main/java/com/group/backend/controller/CronController.java
+++ backend/src/main/java/com/group/backend/controller/CronController.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.group.backend.cron.Cron;
+import com.group.backend.cron.CronApi;
 
 @RestController
 @RequestMapping("cron")
 public class CronController {
-	public final Cron cron;
+	public final CronApi cron;
 
-	public CronController(Cron cron) {
+	public CronController(CronApi cron) {
 		this.cron = cron;
 	}
 
--- backend/src/main/java/com/group/backend/cron/ApiConsumer.java
+++ backend/src/main/java/com/group/backend/cron/ApiConsumer.java
@@ -1,8 +1,23 @@
 package com.group.backend.cron;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group.backend.domain.ResultApiRepository;
+import com.group.backend.entity.ApiPublica;
+import com.group.backend.entity.ResultApi;
+
 public class ApiConsumer {
+
+	ResultApiRepository resultApiRepository;
+	public ApiConsumer(ResultApiRepository resultApiRepository) {
+		this.resultApiRepository = resultApiRepository;
+	}
+
 	public static String getData(String url) {
 		
 		try {
@@ -13,4 +28,40 @@ public class ApiConsumer {
 			return null;
 		}
 	}
+
+	public ResultApi saveNew (ApiPublica api, String data) {
+		ResultApi result = new ResultApi();
+		result.setApiPublica(api);
+		result.setResData(LocalDate.now());
+		result.setResPayload(data);
+		return resultApiRepository.save(result);
+	}
+
+	public void apiRegister(ArrayList<ApiPublica> apiList) {
+		for (ApiPublica api : apiList) {
+			String data = ApiConsumer.getData(api.getUrl());
+			if (data == null) continue;
+
+			ResultApi lastResult = resultApiRepository.getLastFromResId(api.getId());
+			if(lastResult == null) {
+				saveNew(api, data);
+				continue;
+			}
+
+			try {
+				ObjectMapper mapper = new ObjectMapper();
+				JsonNode dataNode = mapper.readTree(data);
+				JsonNode lastPayloadNode = mapper.readTree(lastResult.getResPayload());
+				if (!dataNode.equals(lastPayloadNode)) {
+
+					saveNew(api, data);
+	
+					System.out.println("Saving new data! Updates - " + api.getNome());
+				}
+			} catch (Exception e) {}
+
+			
+		}
+	}
+
 }
