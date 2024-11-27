a9b3d8f7c1288d7f60e07e45f97747054164aaea
GuilhermebJunqueira <143050492+GuilhermebJunqueira@users.noreply.github.com>
2024-11-27 19:49:07 -0300

    Correção Funcionamento css

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -25,20 +25,34 @@
           <option v-for="portal in portais" :key="portal.name" :value="portal.name">{{ portal.name }}</option>
         </select>
       </div>
+      <div class="pagination-wrapper">
+        <div v-if="true" class="pagination-controls">
+          <button :disabled="pageIndex <= 0" @click="() => {
+            this.pageIndex --
+            this.fetchNoticias()
+          }">Anterior</button>
+          <span>Página {{ pageIndex + 1 }}</span>
+          <button :disabled="pageIndex + 1 >= totalPages" @click="() => {
+            this.pageIndex ++
+            this.fetchNoticias()
+          }">Próxima</button>
+        </div>
+      </div>
 
       <!-- Lista de notícias -->
-      <div class="news-list">
-        <NewsCard v-for="noticia in filteredNoticias" :key="noticia.titulo" :noticia="noticia" />
+      <div class="news-list" :key="filteredNoticias">
+        <NewsCard v-for="noticia in filteredNoticias" :key="noticia" :noticia="noticia" />
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
-import '@/assets/base.css';
 import SearchBar from '@/components/SearchBar.vue';
 import DataRange from '@/components/DataRange.vue';
 import NewsCard from '@/components/NewsCard.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -48,6 +62,8 @@ export default {
   },
   data() {
     return {
+      pageIndex: 0,
+      totalPages: 0,
       selectedTag: '',  
       selectedPortal: '',  
       tags: [
@@ -84,7 +100,7 @@ export default {
     };
   },
   mounted() {
-    this.filteredNoticias = this.noticias;
+    this.fetchNoticias()
   },
   methods: {
     handleSearch(keyword) {
@@ -125,7 +141,121 @@ export default {
                this.filterByPortal(noticia) &&
                this.filterByDate(noticia);
       });
+    },
+    async fetchNoticias() {
+      const noticiaList = (await axios(`http://localhost:8080/noticias/listar/${this.pageIndex}`)).data
+      const mappedNoticiaList = noticiaList.map((n) => ({
+        titulo: n.portal.nome,
+        portal: n.portal.nome,
+        jornalista: n.reporte.nome ?? '',
+        content: n.notiText,
+        data: n.notiData ? n.notiData.join('/') : '',
+        categorias: n.tagNoticia.map(tagNoticia => tagNoticia.tagId.tagNome)
+      }))
+      
+      const count = (await axios('http://localhost:8080/noticias/total')).data
+      
+      this.totalPages = Math.ceil(count / 10)
+      this.noticias = mappedNoticiaList
+      this.filteredNoticias = this.noticias;
+      
     }
   }
 };
 </script>
+
+<style scoped>
+.home-view {
+  display: flex;
+  height: 100vh;
+}
+
+.main-content {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  padding: 20px;
+  margin-top: 15px; /* Removida a margem da esquerda */
+}
+
+.search-section {
+  display: flex;
+  gap: 10px;
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.search-bar {
+  flex: 2;
+}
+
+.date-section {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+}
+
+.filters {
+  display: flex;
+  gap: 10px;
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.filters select {
+  flex: 1;
+  width: 50%;
+  padding: 10px;
+  border-radius: 4px;
+  border: 1px solid #ccc; 
+  outline: none; 
+}
+
+.news-list {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.pagination-wrapper {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.pagination-controls {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  margin-bottom: 16px;
+}
+
+.pagination-controls button {
+  padding: 8px 16px;
+  background-color: #6f3c91;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.pagination-controls button:disabled {
+  background-color: #e0e0e0;
+  cursor: not-allowed;
+}
+
+@media (max-width: 768px) {
+  .main-content {
+    padding: 10px;
+    margin-left: 0;
+  }
+
+  .search-section, .filters {
+    flex-direction: column;
+  }
+
+  .filters select {
+    width: 100%;
+  }
+}
+</style>
